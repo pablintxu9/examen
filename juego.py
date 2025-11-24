a3fac0e2b27afacb31b8c93294c441327d1975a0
pablo <pablo.diazgrenufiguerolaferrtt@usp.ceu.es>
2025-11-24 18:20:19 +0100

    tre

--- juego.py
+++ juego.py
@@ -1,19 +1,27 @@
 class personaje:
-    def __init__(self, vida,aguante,inteligencia,turno:bool=True):
+    def __init__(self, vida,aguante,inteligencia):
         self.vida=vida
         self.aguante=aguante
         self.inteligencia=inteligencia
-        self.turno=turno
     def __str__(self):
-        return f"vida: {self.vida}\naguante: {self.aguante} \ninteligencia: {self.inteligencia} \nturno: {self.turno}"
+        return f"vida: {self.vida}\naguante: {self.aguante} \ninteligencia: {self.inteligencia}"
+    def subir_nivel(self):
+        print("Elija qué atributo desea subir de nivel:")
+        a = int(input("1. vida\n2. aguante\n3. inteligencia\n> "))
+        if a == 1:
+           self.vida += 10
+        elif a == 2:
+           self.aguante += 10
+        elif a == 3:
+           self.inteligencia += 10
+        else:
+           print("Opción no válida")
     
-class guerrero(personaje):
-    def __init__(self):
-        super().__init__(vida=150,aguante=100,inteligencia=50)
+ 
+    
 
-class hechicero(personaje):
-    def __init__(self):
-        super().__init__(vida=100,aguante=50,inteligencia=150)
-
-print(guerrero())
-print(hechicero())
+guerrero=personaje(100,80,30)
+mago=personaje(70,50,100)
+print(guerrero)
+mago.subir_nivel()
+print(mago)
--- ordianario/lista_diccionario.py
+++ ordianario/lista_diccionario.py
@@ -5,5 +5,10 @@ ventas = [
     {"id": 3, "region": "Europa", "importe": 200}, 
     {"id": 4, "region": "Asia", "importe": 150} 
 ]
-
-def 
+def total_ventas_por_region(ventas, region):
+    total = 0
+    for venta in ventas:
+        if venta["region"] == region:
+            total += venta["importe"]
+    return total
+print (total_ventas_por_region(ventas, "Europa"))  # Output: 300
