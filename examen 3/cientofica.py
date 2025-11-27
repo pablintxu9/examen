4caba5c64b22bc9c6934e32be6fc21107bb10393
pablo <pablo.diazgrenufiguerolaferrtt@usp.ceu.es>
2025-11-27 17:05:30 +0100

    dg

--- examen 3/cientofica.py
+++ examen 3/cientofica.py
@@ -1,14 +1,22 @@
 import numpy as np 
-import matplotlib.pyplot as plt 
+import matplotlib.pyplot as ptl 
  
 prof = [] 
 temp = [] 
 
 with open("mues_marias.csv") as archivo:
     for linea in archivo:
+        try :
         datos = linea.split()
         prof.append(float(datos[0]))
         temp.append(float(datos[1]))
-    
+        except ValueError:
+        print ("Dato no valido, se omite la linea")
+        continue
+
 prof = np.array(prof)
 temp = np.array(temp)
+print(np.median(temp))
+print(np.array(temp)<20)
+
+ptl.scatter(temp, prof)
